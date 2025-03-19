8b486301b104695e06c33aba8f04d470256ce420
gowtamkumar <gowtampaul0@gmail.com>
2025-03-19 08:09:30 +0600

    time complexity

---
+++ dsa/typescript/src/complexity/time-complextiy.ts
@@ -0,0 +1,13 @@
+// 1. assienment oparator - 0(1)
+// 2. comparision operator - 0(1)
+// 3. Mathematical operators - 0(1)
+// 4. function called- 0(1)
+// 5. work in function - 0(1)
+// thouse are the basic operation that we can count as a time complexity
+// when take one oparation 0(1) time complexity thery nem bio O(1)
+
+// n * n+1 / 2  here O(1) time complexity
+// if we calculation by loop then it will be O(n) time complexity
+// for (let i = 0; i < n; i++) {
+//     console.log(i);
+// }
