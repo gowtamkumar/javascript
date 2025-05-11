fc6e920f826bfcbc2edb27218a952bf023c72f24
gowtamkumar <gowtampaul0@gmail.com>
2025-05-11 07:29:32 +0600

    binner search

--- dsa/typescript/src/search-algorithom/binarySerach.ts
+++ dsa/typescript/src/search-algorithom/binarySerach.ts
@@ -1,28 +1,26 @@
 // binary search should be sorted
-
 let newArray = [1, 3, 4, 6, 7, 12, 33, 44, 56, 76, 81, 91, 92, 95];
-let left = 0;
-let right = newArray.length - 1;
-let findValues = null;
-let targetValue = 3;
 
-while (left <= right) {
-  let mid = Math.floor((left + right) / 2);
+let targetValue: number = 1;
 
-  if (newArray[mid] === targetValue) {
-    findValues = newArray[mid];
-    break;
-  }
+function binarySearch(newArray: any[], findValue: number) {
+  let right = newArray.length - 1;
+  let left = 0;
 
-  if (newArray[mid] < targetValue) {
-    left = mid + 1;
-  } else if (newArray[mid] > targetValue) {
-    right = mid - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (newArray[mid] === findValue) {
+      return newArray[mid];
+    }
+
+    if (newArray[mid] < findValue) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
 }
 
-if (findValues !== null) {
-  console.log("dinf valu", findValues);
-} else {
-  console.log("not found this");
-}
+const res = binarySearch(newArray, targetValue);
+
+console.log("res", res);
