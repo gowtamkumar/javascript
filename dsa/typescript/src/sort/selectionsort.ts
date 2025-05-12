7170292258fee500255b30757fed72c9b3e1c941
gowtamkumar <gowtampaul0@gmail.com>
2025-05-12 22:31:56 +0600

    selection sort

--- dsa/typescript/src/sort/selectionsort.ts
+++ dsa/typescript/src/sort/selectionsort.ts
@@ -1,28 +1,20 @@
 const newArray = [1, 3, 13, 2, 4, 55, 22, 11, 5, 100, 99, 12];
+let checkvalue, temp;
 
 for (let i = 0; i < newArray.length; i++) {
-  const element = newArray[i];
-  console.log(element);
+  checkvalue = i;
 
-  let checkvalue = 0;
-  for (let j = 1; j < newArray.length; j++) {
-    console.log(newArray[j]);
-
-    if (newArray[j] > newArray[j + 1]) {
+  for (let j = i + 1; j < newArray.length; j++) {
+    console.log(j);
+    
+    if (newArray[j] < newArray[checkvalue]) {
       checkvalue = j;
-    } else {
-      checkvalue = j + 1;
     }
   }
 
-  const elements = newArray[checkvalue];
-  let store = 0;
-  console.log("checkvalue", checkvalue);
-  // if (element > elements) {
-  //   store = i;
-  //   newArray[i] = newArray[checkvalue];
-  //   newArray[checkvalue] = newArray[store];
-  // }
+  if (checkvalue != i) {
+    temp = newArray[i];
+    newArray[i] = newArray[checkvalue];
+    newArray[checkvalue] = temp;
+  }
 }
-
-console.log("newArray", newArray);
