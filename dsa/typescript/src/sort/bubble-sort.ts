2b966c050166601d8df8dd15fbea44dde58817d5
gowtamkumar <gowtampaul0@gmail.com>
2025-05-14 20:43:02 +0600

    bubble sort

--- dsa/typescript/src/sort/bubble-sort.ts
+++ dsa/typescript/src/sort/bubble-sort.ts
@@ -1,22 +1,14 @@
 const newArray = [1, 3, 13, 2, 4, 55, 22, 11, 5, 100, 99, 12];
-
-for (let i = 0; i < newArray.length; i++) {
-  let temp;
-  const element = newArray[i];
-  if (newArray[i] > newArray[i + 1]) {
-
-    temp = newArray[i];
-    newArray[i] = newArray[i + 1];
-    newArray[i + 1] = temp;
-    // console.log(newArray);
-    
+let len = newArray.length;
+let temp;
+for (let i = 0; i < len; i++) {
+  for (let j = 0; j < len - i - 1; j++) {
+    if (newArray[j] > newArray[j + 1]) {
+      temp = newArray[j];
+      newArray[j] = newArray[j + 1];
+      newArray[j + 1] = temp;
+    }
   }
-
-  console.log("temp", temp);
-  
-
-
-
 }
 
 console.log("newArray", newArray);
