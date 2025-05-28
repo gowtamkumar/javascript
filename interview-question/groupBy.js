39f3039bd124418b1d2382cb10c724351b0719cc
gowtamkumar <gowtampaul0@gmail.com>
2025-05-28 14:27:12 +0600

    object method

---
+++ interview-question/groupBy.js
@@ -0,0 +1,12 @@
+const inventory = [
+  { name: "asparagus", type: "vegetables", quantity: 9 },
+  { name: "bananas", type: "fruit", quantity: 5 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 12 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
+
+const result = Object.groupBy(inventory, ({ type }) => type);
+
+console.log("result", result);
+
---
+++ interview-question/objectis.js
@@ -0,0 +1,2 @@
+const obj = {}
+console.log(Object.is(obj, {}));
