ffca4e6f5efdbef24ebd159b1c3cefcac7fedd23
gowtamkumar <gowtampaul0@gmail.com>
2025-05-28 15:55:27 +0600

    array and date

---
+++ interview-question/array.js
@@ -0,0 +1,21 @@
+// const number = [1, 2, 3, 4, 5, 6];
+
+// console.log(Array.from(number, (a) => a * a));
+// console.log(Array.from("gowtam kumar"));
+
+
+// const zeros = new Array(5).fill(1)
+
+// console.log(zeros);
+
+// const newArray = [5, 12, 50, 130, 44];
+
+
+//  console.log(newArray.findLastIndex((item)=> item));
+ 
+
+// const beasts = ["ant", "bison", "camel", "duck", "bison"];
+
+// console.log(beasts.indexOf("bison"));
+
+
