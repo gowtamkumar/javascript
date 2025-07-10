2a6128076a6b9cdccbfeab1f0bc717a440a10a87
gowtamkumar <gowtampaul0@gmail.com>
2025-07-10 12:19:47 +0600

    start search algorithoms and linear search

---
+++ dsa/javascript/search-algorithms/linear-search.js
@@ -0,0 +1,10 @@
+function linearSearch(arr, value) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === value){
+            return 1;
+        }
+    }
+    return -1;
+}
+
+linearSearch([11,22,3,455,66,5], 11)
