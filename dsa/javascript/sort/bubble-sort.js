92c279c341c916ed7a2f59977abd54e9459d514c
gowtamkumar <gowtampaul0@gmail.com>
2025-07-13 16:08:06 +0600

    optimize this bubble sort

--- dsa/javascript/sort/bubble-sort.js
+++ dsa/javascript/sort/bubble-sort.js
@@ -1,16 +1,24 @@
 function bubbleSort(arr) {
   let len = arr.length;
+  let noSowap;
+  let count = 0
   for (let i = 0; i < len; i++) {
+    noSowap = true;
     for (let j = 0; j < len - i - 1; j++) {
       console.log(arr, arr[j], arr[j + 1]);
-
+      count++
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        noSowap = false;
       }
     }
-  } 
+    if (noSowap) break;
+  }
+
+  console.log(" count++",  count++);
+  
   return arr;
 }
 
