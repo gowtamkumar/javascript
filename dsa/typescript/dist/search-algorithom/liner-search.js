2d3bf64dfeb945c50eec3bcbe09ecc53cd2e0c7d
gowtamkumar <gowtampaul0@gmail.com>
2025-07-14 17:05:15 +0600

    remove ts dsa

--- dsa/typescript/dist/search-algorithom/liner-search.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let array = [2, 33, 44, 55, 10, 300, 500];
-let findValue = 55;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] === findValue) {
-        console.log(i);
-    }
-}
