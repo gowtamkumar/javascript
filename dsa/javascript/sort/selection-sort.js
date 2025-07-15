d8024cb239b6739999f9ebf1d85896183381b5ea
gowtamkumar <gowtampaul0@gmail.com>
2025-07-15 10:44:35 +0600

    implement reverse

--- dsa/javascript/sort/selection-sort.js
+++ dsa/javascript/sort/selection-sort.js
@@ -1,24 +1,43 @@
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    // console.log(element);
     let small = i;
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[small] > arr[j]) {
         small = j;
       }
 
-      let temp = arr[small];
-      arr[small] = arr[i];
-      arr[i] = temp;
+      // let temp = arr[small];
+      // arr[small] = arr[i];
+      // arr[i] = temp;
+      [arr[small], arr[i]] = [arr[i], arr[small]];
     }
   }
-  console.log(arr);
-
   return arr;
 }
 
 console.log(
-  "selectionSort([2, 3, 1, 3, 55, 100, 500, 22, 11, 33, 44, 55]);",
-  selectionSort([2, 3, 3, 1, 55, 100, 500, 22, 11, 33, 44, 55,1,1,1,1111,11])
+  "selection sort",
+  selectionSort([
+    2, 3, 3, 1, 55, 100, 500, 22, 11, 33, 44, 55, 1, 1, 1, 1111, 11,
+  ])
+);
+
+function selectionSortRevese(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let large = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] > arr[large]) {
+        large = j;
+      }
+    }
+    [arr[i], arr[large]] = [arr[large], arr[i]];
+  }
+  return arr;
+}
+
+console.log(
+  "selectionSort reverse",
+  selectionSortRevese([
+    2, 3, 3, 1, 55, 100, 500, 22, 11, 33, 44, 55, 1, 1, 1, 1111, 11,
+  ])
 );
