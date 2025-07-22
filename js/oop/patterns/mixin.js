e4a033970edf914de1623822bcc776d7a770dec1
gowtamkumar <gowtampaul0@gmail.com>
2025-07-22 16:06:33 +0600

    mixin pattern func

---
+++ js/oop/patterns/mixin.js
@@ -0,0 +1,23 @@
+const sayHIMixin = {
+  sayHi() {
+    console.log(`hi, my name is ${this.name}`);
+  },
+};
+
+const sayByeMixin = {
+  sayBye() {
+    console.log(`Bye from ${this.name}`);
+  },
+};
+
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+Object.assign(User.prototype, sayHIMixin, sayByeMixin);
+
+const user = new User("Gowtam kumar");
+user.sayHi()
+user.sayBye()
