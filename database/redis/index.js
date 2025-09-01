409490c1461745f345eb6d72ec44786a2f02a99d
gowtamkumar <gowtampaul0@gmail.com>
2025-09-01 14:07:48 +0600

    ioredis set data

--- database/redis/index.js
+++ database/redis/index.js
@@ -1,7 +1,8 @@
 const client = require("./config");
 
 async function init() {
-  await client.set("data:1", "hello boss hwo old are you?");
+  const newDATa = { name: "gowtam kumar", age: 33 };
+  await client.set("data:1", JSON.stringify(newDATa));
   client.expire("data:1", 10);
   const result = await client.get("data:1");
   console.log("asdfasdf", result);
