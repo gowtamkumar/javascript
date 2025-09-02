f1c1d5591325329364da65541960afcac4ec49f7
gowtamkumar <gowtampaul0@gmail.com>
2025-09-02 10:58:25 +0600

    doubly link push

---
+++ dsa/javascript/data-structure/doubly-linked-list.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const result = new DoublyLinkedList();
+
+result.push(44);
+result.push(50);
+
+console.log("lsit all", result);
