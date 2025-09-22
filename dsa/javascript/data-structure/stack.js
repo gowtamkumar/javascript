3caa12a6a580025b3d469ba9d150961954664600
gowtamkumar <gowtampaul0@gmail.com>
2025-09-22 10:14:11 +0600

    stack

---
+++ dsa/javascript/data-structure/stack.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    this.size++;
+    return this;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const result = new Stack();
+
+result.push("hello world");
+result.push("I learn DSA");
+result.pop();
+result.pop();
+console.log(result);
