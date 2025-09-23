b7cfabdd846c9d281e47b2cdcf576e6a940e3b1a
gowtamkumar <gowtampaul0@gmail.com>
2025-09-23 10:01:51 +0600

    dsa topic queue

---
+++ dsa/javascript/data-structure/queue.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+    return this;
+  }
+  dequeue() {
+    if (!this.first) return null;
+    const temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+const resultQuuee = new Queue();
+resultQuuee.enqueue("hwo old are you");
+resultQuuee.enqueue("hello");
+resultQuuee.dequeue();
+resultQuuee.dequeue();
+
+console.log("resultQuuee", resultQuuee);
