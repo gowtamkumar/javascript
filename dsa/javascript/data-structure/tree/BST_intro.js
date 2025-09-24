fce1e4f188ca655f1b3823fae8aa76ce540e3e32
gowtamkumar <gowtampaul0@gmail.com>
2025-09-24 10:19:29 +0600

    binary search tree

---
+++ dsa/javascript/data-structure/tree/BST_intro.js
@@ -0,0 +1,15 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarrySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+const resutl = new BinarrySearchTree();
