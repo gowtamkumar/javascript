f5527951410de659cc0bbc819fd5f0cb1f5eac06
gowtamkumar <gowtampaul0@gmail.com>
2025-09-25 09:56:17 +0600

    binarry search insert

--- dsa/javascript/data-structure/tree/BST_intro.js
+++ dsa/javascript/data-structure/tree/BST_intro.js
@@ -10,6 +10,46 @@ class BinarrySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            // here update current
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            // here update current
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
-const resutl = new BinarrySearchTree();
+const result = new BinarrySearchTree();
+
+result.insert(33);
+result.insert(10);
+result.insert(9);
+result.insert(100);
+result.insert(110);
+result.insert(99);
+
+console.log(result);
