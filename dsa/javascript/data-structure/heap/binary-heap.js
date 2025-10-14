9a6aacb4a74779b4f0b9c3004e05ea763cc1765e
gowtamkumar <gowtampaul0@gmail.com>
2025-10-14 09:54:54 +0600

    binary heap insert

--- dsa/javascript/data-structure/heap/binary-heap.js
+++ dsa/javascript/data-structure/heap/binary-heap.js
@@ -1,1 +1,31 @@
 // ## binary heap
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [41, 39, 33, 18, 27, 12, 55];
+  }
+
+  insert(value) {
+    this.values.push(value);
+    this.boubbleUp();
+  }
+  boubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parnetId = Math.floor((idx - 1) / 2); //this parnet data find math
+      const parentData = this.values[parnetId];
+      if (element <= parentData) break;
+      this.values[parnetId] = element;
+      this.values[idx] = parentData;
+      idx = parnetId;
+    }
+  }
+}
+
+const heap = new MaxBinaryHeap();
+
+heap.insert(55);
+heap.insert(100);
+heap.insert(1);
+
+console.log("heap", heap);
