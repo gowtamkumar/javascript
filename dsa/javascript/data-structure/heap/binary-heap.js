00fbe3c89b4825948e078b931f0d90a10c4adbd5
gowtamkumar <gowtampaul0@gmail.com>
2025-10-15 10:14:55 +0600

    binary heap and heap Extract max

--- dsa/javascript/data-structure/heap/binary-heap.js
+++ dsa/javascript/data-structure/heap/binary-heap.js
@@ -20,6 +20,50 @@ class MaxBinaryHeap {
       idx = parnetId;
     }
   }
+
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > element)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) {
+        break;
+      }
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
@@ -27,5 +71,10 @@ const heap = new MaxBinaryHeap();
 heap.insert(55);
 heap.insert(100);
 heap.insert(1);
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
 
 console.log("heap", heap);
