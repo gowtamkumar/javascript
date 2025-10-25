a00671b94cbe7399c5af26e3c2f6ed4b73e716fb
gowtamkumar <gowtampaul0@gmail.com>
2025-10-25 10:25:52 +0600

    hash fun

---
+++ dsa/javascript/data-structure/hash-table/hash-fun.js
@@ -0,0 +1,13 @@
+function hash(key, arraylength) {
+  let total = 0;
+  let WEIRD_PRIME = 31;
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i];
+    let value = char.charCodeAt(0) - 96;
+    total = (total * WEIRD_PRIME + value) % arraylength;
+  }
+  return total;
+}
+
+const res = hash("hello world", 13);
+console.log(res);
