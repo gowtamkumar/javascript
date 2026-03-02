2187daf2e0b40074184c9c383311bdd38190d657
gowtamkumar <gowtampaul0@gmail.com>
2026-03-02 10:24:18 +0600

    number sum and number reverse

--- dsa/index.js
+++ dsa/index.js
@@ -1,65 +1,65 @@
 // # type coercion
 
-console.log("1" + 10); // 110
-console.log("1" - 1); //0
-console.log("1" * 1); // 1
+// console.log("1" + 10); // 110
+// console.log("1" - 1); //0
+// console.log("1" * 1); // 1
 
-// let age = prompt("Enter you age");
+// // let age = prompt("Enter you age");
 
-// console.log("age", age);
+// // console.log("age", age);
 
-// # type casting
-// one type to other type convert is type casting
+// // # type casting
+// // one type to other type convert is type casting
 
-// # swap
-let a = 10;
-let b = 20;
-let c;
-c = a; // copy c = 10
-a = b; // a = 20
-b = c; // b = 10
+// // # swap
+// let a = 10;
+// let b = 20;
+// let c;
+// c = a; // copy c = 10
+// a = b; // a = 20
+// b = c; // b = 10
 
-console.log("a: ", a, "b: ", b);
-// swap without extra variable
-let d = 20;
-let e = 10;
+// console.log("a: ", a, "b: ", b);
+// // swap without extra variable
+// let d = 20;
+// let e = 10;
 
-d = d + e; // d= 30
-e = d - e; // e = 20
-d = d - e; // d = 10
-console.log("d: ", d, "e: ", e);
+// d = d + e; // d= 30
+// e = d - e; // e = 20
+// d = d - e; // d = 10
+// console.log("d: ", d, "e: ", e);
 
-// es6 swap
+// // es6 swap
 
-let j = 100;
-let k = 200;
+// let j = 100;
+// let k = 200;
 
-[j, k] = [k, j]; // array destructuring assignment and ES6 destructuring
+// [j, k] = [k, j]; // array destructuring assignment and ES6 destructuring
 
-console.log("J: ", j, "K: ", k);
+// console.log("J: ", j, "K: ", k);
 
 // # oparator
 //# division
-let f = 12;
-let g = 22;
-console.log(f / g); // j ke division to g
-console.log(g / j);
-console.log(Math.floor(f / g)); // j ke division to g
-console.log(Math.floor(g / j));
+// let f = 12;
+// let g = 22;
+// console.log(f / g); // j ke division to g
+// console.log(g / j);
+// console.log(Math.floor(f / g)); // j ke division to g
+// console.log(Math.floor(g / j));
 // # Modulus(made)
-console.log("Modulus: ", g % f); // 10
-console.log("Modulus: ", f % g); // here can not divide
-// # relation operator ">,<,<=,>=,!=, !=="
-// logical operator "&&,||"
-console.log(10 > 6 && 5 < 9); // if two condition true then return true
-console.log(10 > 6 && 5 < 9 && 5 > 20); // if two condition true then return true and if this check left to right if find a flase then return false
+// console.log("Modulus: ", g % f); // 10
+// console.log("Modulus: ", f % g); // here can not divide
+// // # relation operator ">,<,<=,>=,!=, !=="
+// // logical operator "&&,||"
+// console.log(10 > 6 && 5 < 9); // if two condition true then return true
+// console.log(10 > 6 && 5 < 9 && 5 > 20); // if two condition true then return true and if this check left to right if find a flase then return false
 // # unary operator "++,--"
 //  1. post increment
 //  2. pre increment
 //  1. post deincrement
 //  2. pre decrement
 
-let i = 10;
+// let i = 10;
 // console.log(i++); // 10 post increment
 // console.log(i); // 11
 // console.log(++i); // 12 pre increment
@@ -70,7 +70,7 @@ let i = 10;
 // console.log(--i); //8 pre decrement
 // console.log(i); //
 
-let h = true; // true is 1
+// let h = true; // true is 1
 // console.log(++h);
 
 // example 1
@@ -81,72 +81,72 @@ let h = true; // true is 1
 // console.log(kg);
 
 // # Math
-console.log(Math.round(10.3));
-console.log(Math.ceil(10.3));
-console.log(Math.floor(22.6));
-console.log(Math.trunc(23.4));
-console.log(Math.pow(2, 5));
-console.log(Math.sqrt(69));
-console.log(Math.cbrt(27)); // here result 3 mean 3 x 3 x 3
-console.log(Math.abs(-33)); // this remove to nagative to positive convert
-console.log(Math.max(33, 44, 100));
-console.log(Math.trunc(Math.random() * 9000) + 1000); // opt number create
+// console.log(Math.round(10.3));
+// console.log(Math.ceil(10.3));
+// console.log(Math.floor(22.6));
+// console.log(Math.trunc(23.4));
+// console.log(Math.pow(2, 5));
+// console.log(Math.sqrt(69));
+// console.log(Math.cbrt(27)); // here result 3 mean 3 x 3 x 3
+// console.log(Math.abs(-33)); // this remove to nagative to positive convert
+// console.log(Math.max(33, 44, 100));
+// console.log(Math.trunc(Math.random() * 9000) + 1000); // opt number create
 
 // # condition statement
-let amount = 2909;
+// let amount = 2909;
 
-if (amount >= 500) {
-  console.log("500 notes: " + Math.floor(amount / 500));
-  amount = amount % 500;
-}
+// if (amount >= 500) {
+//   console.log("500 notes: " + Math.floor(amount / 500));
+//   amount = amount % 500;
+// }
 
-if (amount >= 200) {
-  console.log("200 notes: " + Math.floor(amount / 200));
-  amount = amount % 200;
-}
-if (amount >= 100) {
-  console.log("100 notes: " + Math.floor(amount / 100));
-  amount = amount % 100;
-}
-if (amount >= 50) {
-  console.log("50 notes: ", Math.floor(amount / 50));
-  amount = amount % 50;
-}
+// if (amount >= 200) {
+//   console.log("200 notes: " + Math.floor(amount / 200));
+//   amount = amount % 200;
+// }
+// if (amount >= 100) {
+//   console.log("100 notes: " + Math.floor(amount / 100));
+//   amount = amount % 100;
+// }
+// if (amount >= 50) {
+//   console.log("50 notes: ", Math.floor(amount / 50));
+//   amount = amount % 50;
+// }
 
-if (amount >= 20) {
-  console.log("20 notes: ", Math.floor(amount / 20));
-  amount = amount % 20;
-}
-if (amount >= 10) {
-  console.log("10 notes: ", Math.floor(amount / 10));
-  amount = amount % 10;
-}
+// if (amount >= 20) {
+//   console.log("20 notes: ", Math.floor(amount / 20));
+//   amount = amount % 20;
+// }
+// if (amount >= 10) {
+//   console.log("10 notes: ", Math.floor(amount / 10));
+//   amount = amount % 10;
+// }
 
-if (amount >= 5) {
-  console.log("5 notes: ", Math.floor(amount / 5));
-  amount = amount % 5;
-}
-if (amount >= 2) {
-  console.log("2 notes: ", Math.floor(amount / 2));
-  amount = amount % 2;
-}
+// if (amount >= 5) {
+//   console.log("5 notes: ", Math.floor(amount / 5));
+//   amount = amount % 5;
+// }
+// if (amount >= 2) {
+//   console.log("2 notes: ", Math.floor(amount / 2));
+//   amount = amount % 2;
+// }
 
-console.log("1 notes", amount);
+// console.log("1 notes", amount);
 
-let num = Number((0.1 + 0.2).toFixed(1));
-newday = "";
+// let num = Number((0.1 + 0.2).toFixed(1));
+// newday = "";
 
-switch (num) {
-  case 0.3:
-    newday = "Tuesday";
-    break;
-  case 3.5:
-    newday = "Friday";
-  default:
-    newday = "Invalid day";
-}
+// switch (num) {
+//   case 0.3:
+//     newday = "Tuesday";
+//     break;
+//   case 3.5:
+//     newday = "Friday";
+//   default:
+//     newday = "Invalid day";
+// }
 
-console.log("newday", newday);
+// console.log("newday", newday);
 
 // # loop
 // reverse loop
@@ -155,36 +155,73 @@ console.log("newday", newday);
 // step 2: i > 100
 // step 3: {console.log(i)}
 // step:4  i--
-let ik = 200;
-for (; ik > 100; ik--) {
-  // 200 to 100
-  console.log(ik);
-}
+// let ik = 200;
+// for (; ik > 100; ik--) {
+//   // 200 to 100
+//   console.log(ik);
+// }
 
-console.log("fail", ik);
+// console.log("fail", ik);
 
 // # Factors of number
-let n = 12;
-for (let i = 1; i <= Math.floor(n / 2); i++) {
-  if (n % i === 0) {
-    console.log("teting...", i);
-  }
-}
-console.log("teting...", n);
+// let n = 12;
+// for (let i = 1; i <= Math.floor(n / 2); i++) {
+//   if (n % i === 0) {
+//     console.log("teting...", i);
+//   }
+// }
+// console.log("teting...", n);
 // # prime number exam: 1, self can be divide this is prime number
 
-function isPrimeNumber(n) {
-  if (n <= 1) return false;
-  if (n % 2 === 0) return false;
-  if (n == 2) return true;
-  let isPrime = true;
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      isPrime = false;
-      break;
-    }
-  }
-  return isPrime;
-}
+// function isPrimeNumber(n) {
+//   if (n <= 1) return false;
+//   if (n % 2 === 0) return false;
+//   if (n == 2) return true;
+//   let isPrime = true;
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 0) {
+//       isPrime = false;
+//       break;
+//     }
+//   }
+//   return isPrime;
+// }
 
-console.log(isPrimeNumber(10));
+// console.log(isPrimeNumber(10));
+
+// # while vs for
+// for (start; end; chnage) {}
+// start;
+// while (end) {
+//   chnage;
+// }
+
+// let whtes = 1;
+// while (whtes < 23) {
+//   console.log("while", whtes);
+//   whtes++;
+// }
+
+// # sum of digit
+// let sumdigit = 123;
+// let sum = 0;
+// while (sumdigit > 0) {
+//   console.log(sumdigit % 10);
+//   let rem = sumdigit % 10;
+//   sum = sum + rem;
+//   sumdigit = Math.floor(sumdigit / 10);
+//   console.log(Math.floor(sumdigit / 10));
+// }
+// console.log("sum", sum);
+// # reverse number
+let n = 123456789;
+rev = 0;
+while (n > 0) {
+  let rem = n % 10;
+  console.log("rem", rem);
+  rev = rev * 10 + rem;
+  console.log("rev", rev);
+  console.log("division", Math.floor(n / 10));
+  n = Math.floor(n / 10);
+}
+console.log("reverse", rev);
