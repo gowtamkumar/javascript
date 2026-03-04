c0c7478f2c00a5b2a7f40fbad6c4ba5cc8c15048
gowtamkumar <gowtampaul0@gmail.com>
2026-03-04 14:51:02 +0600

    x pattern

--- dsa/pattern/index.js
+++ dsa/pattern/index.js
@@ -1,5 +1,5 @@
 const prompt = require("prompt-sync")();
-let n = prompt("Enter a number");
+let n = Number(prompt("Enter a number"));
 
 // for (let i = 1; i <= pro; i++) {
 //   for (let j = 1; j <= pro; j++) {
@@ -47,13 +47,26 @@ let n = prompt("Enter a number");
 
 // # home work full traningle
 
-for (let i = 1; i <= n; i++) {
-  for (let s = 1; s <= n - i; s++) {
-    process.stdout.write(" ");
-  }
+// for (let i = 1; i <= n; i++) {
+//   for (let s = 1; s <= n - i; s++) {
+//     process.stdout.write(" ");
+//   }
 
-  for (let j = 1; j <= 2 * i - 1; j++) {
-    process.stdout.write("*");
+//   for (let j = 1; j <= 2 * i - 1; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log("");
+// }
+
+// # x pattern
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == j || i + j == n + 1) {
+      process.stdout.write("* ");
+    } else {
+      process.stdout.write("  "); // TWO spaces
+    }
   }
-  console.log("");
+  console.log();
 }
