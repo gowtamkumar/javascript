9e3fe9b92e9e7b63d96cf912657515bb5ddb01e8
gowtamkumar <gowtampaul0@gmail.com>
2026-03-05 16:10:54 +0600

    array with problem solve

---
+++ dsa/array.js
@@ -0,0 +1,33 @@
+// array reverse
+// let newArry = [1, 2, 3, 4, 5, 6, 7];
+// let first = 0;
+// let last = newArry.length - 1;
+// let temp = 0;
+// let len = newArry.length;
+// for (let i = 0; i < len; i++) {
+//   temp = newArry[i];
+//   newArry[i] = newArry[last];
+//   newArry[last] = temp;
+//   first++;
+//   last--;
+//   len--;
+//   len--;
+// }
+
+// console.log("newArry", newArry);
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let first = 0;
+let last = arr.length - 1;
+
+while (first < last) {
+  let temp = arr[first];
+
+  arr[first] = arr[last];
+  arr[last] = temp;
+
+  first++;
+  last--;
+}
+
+console.log(arr);
