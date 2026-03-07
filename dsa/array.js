8c42cc9fa15ab3b5c4f02202408bc9c1879791f3
gowtamkumar <gowtampaul0@gmail.com>
2026-03-07 15:43:50 +0600

    array recap

--- dsa/array.js
+++ dsa/array.js
@@ -33,3 +33,32 @@
 // console.log(arr);
 
 // # advance array
+// let arr = [1, 2, 3, 4, 5, 6]; result: [6, 2, 3, 4, 5, 1];
+// let copy = arr[0];
+// for (let i = 0; i < arr.length - 1; i++) {
+//   console.log("i + 1", arr.length - 1);
+
+//   arr[i] = arr[i + 1];
+// }
+// arr[arr.length - 1] = copy;
+// console.log("arr", arr);
+// let arr = [1, 2, 3, 4, 5, 6]; // result: [6, 1, 2, 3, 4, 5];
+// let copy = arr[arr.length - 1];
+// for (let i = arr.length - 1; i > 0; i--) {
+//   arr[i] = arr[i - 1];
+//   console.log(i);
+// }
+// arr[0] = copy;
+// console.log("arr", arr);
+
+let arr = [1, 2, 3, 4, 5, 6]; // result k step left
+let k = 2;
+for (let j = 0; j < k; j++) {
+  let copy = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[arr.length - 1] = copy;
+}
+
+console.log("arr", arr);
