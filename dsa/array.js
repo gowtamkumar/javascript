c16765e3dfc7a45c746d5eecd2c76534f7eb9d94
gowtamkumar <gowtampaul0@gmail.com>
2026-03-14 10:21:54 +0600

    sort array

--- dsa/array.js
+++ dsa/array.js
@@ -98,7 +98,7 @@
 
 // console.log("marge", marge);
 
-let arr1 = [2, 5, 5, 7, 8, 1, 100, 0];
+// let arr1 = [2, 5, 5, 7, 8, 1, 100, 0];
 // let dublicate = [];
 // let k = 0,
 //   i = 0;
@@ -114,18 +114,44 @@ let arr1 = [2, 5, 5, 7, 8, 1, 100, 0];
 // console.log("dublicate", dublicate);
 
 // # min value and max
+// let arr1 = [2, 5, 5, 7, 8, 1, 100, 0];
+// let min = arr1[0];
+// let max = arr1[0];
+// for (let i = 0; i < arr1.length; i++) {
+//   if (min > arr1[i]) {
+//     // min
+//     min = arr1[i];
+//   }
+//   if (min < arr1[i]) {
+//     max = arr1[i];
+//   }
+// }
 
-let min = arr1[0];
-let max = arr1[0];
-for (let i = 0; i < arr1.length; i++) {
-  if (min > arr1[i]) {
-    // min
-    min = arr1[i];
-  }
-  if (min < arr1[i]) {
-    max = arr1[i];
+// console.log("min", min);
+// console.log("max", max);
+
+let arry = [1, 2, 0, 1, 0, 1, 2, 0, 0, 2];
+
+let i = 0;
+let j = 0;
+let k = arry.length - 1;
+let temp;
+
+while (i <= k) {
+  if (arry[i] === 0) {
+    temp = arry[i];
+    arry[i] = arry[j];
+    arry[j] = temp;
+    i++;
+    j++;
+  } else if (arry[i] === 2) {
+    temp = arry[i];
+    arry[i] = arry[k];
+    arry[k] = temp;
+    k--;
+  } else {
+    i++;
   }
 }
 
-console.log("min", min);
-console.log("max", max);
+console.log("arry", arry);
