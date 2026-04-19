5f914d2239342a7d3acb237a8fe77eeb321c0c01
gowtamkumar <gowtampaul0@gmail.com>
2026-04-19 13:15:24 +0600

    array

--- dsa/array/index.js
+++ dsa/array/index.js
@@ -38,16 +38,82 @@
 // console.log("arr", arr);
 // Two Sum // target = 9
 
-const nums = [2, 11, 7, 15];
-let sum = [];
-let target = 9;
-for (let i = 0; i < nums.length; i++) {
-  for (let j = i + 1; j < nums.length; j++) {
-    if (nums[i] + nums[j] === target) {
-      sum = [i, j];
-      break;
-    }
-  }
-}
+// const nums = [2, 11, 7, 15];
+// let sum = [];
+// let target = 9;
+// for (let i = 0; i < nums.length; i++) {
+//   for (let j = i + 1; j < nums.length; j++) {
+//     if (nums[i] + nums[j] === target) {
+//       sum = [i, j];
+//       break;
+//     }
+//   }
+// }
 
-console.log("sum", sum);
+// console.log("sum", sum);
+
+// const nums = [2, 11, 7, 15];
+// function twoSum(arr, target) {
+//   const map = new Map();
+//   for (let i = 0; i < arr.length; i++) {
+//     let diff = target - arr[i];
+//     console.log("diff", diff);
+//     console.log("map", map);
+
+//     if (map.has(diff)) {
+//       return [map.get(diff), i];
+//     }
+//     map.set(arr[i], i);
+//   }
+//   return [];
+// }
+
+// console.log(twoSum(nums, 9));
+
+// const nums = [2, 11, 7, 15];
+// function addFirst(nums, value) {
+//   console.log("length", nums.length);
+
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     nums[i + 1] = nums[i];
+//   }
+//   nums[0] = value;
+// }
+
+// addFirst(nums, 90);
+
+// function lastAdd(nums, value) {
+//   nums[nums.length] = value;
+// }
+// lastAdd(nums, 30);
+// lastAdd(nums, 88);
+
+// console.log("nums", nums);
+
+//middle array insert
+// const nums = [2, 11, 7, 15];
+// function addMid(nums, value) {
+//   let mid = Math.floor(nums.length / 2);
+//   for (let i = nums.length - 1; i >= mid; i--) {
+//     nums[i + 1] = nums[i];
+//   }
+
+//   nums[mid] = value;
+// }
+
+// addMid(nums, 90);
+
+// console.log(nums);
+
+// const nums = [2, 11, 7, 15];
+
+// function insert(arr, index, value) {
+//   for (let i = arr.length - 1; i >= index; i--) {
+//     arr[i + 1] = arr[i];
+//   }
+//   arr[index] = value;
+// }
+
+// insert(nums, 2, 33);
+
+// console.log("nums", nums);
--- dsa/doc.md
+++ dsa/doc.md
@@ -1,3 +1,23 @@
-# guildline dsa:
+🧠 2. Step-by-step Intuition (How to Think)
 
-1. language
+When solving array problems, always think in this order:
+
+Step 1: Understand the problem
+What is input?
+What is expected output?
+Step 2: Identify pattern
+
+Most array problems fall into these patterns:
+
+Linear scan (loop)
+Two pointers
+Sliding window
+Prefix sum
+Step 3: Ask key questions
+Do I need all elements or just part?
+Is order important?
+Is array sorted?
+Can I use extra memory (like object/map)?
+Step 4: Optimize
+
+Start with brute force → then improve
